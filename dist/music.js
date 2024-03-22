8196a5f8f4285e2991832085f42341c0c3371a76
jasonkayzk <271226192@qq.com>
2024-03-22 08:21:02 +0000

    Site updated: 2024-03-22 08:21:00

---
+++ dist/music.js
@@ -0,0 +1,31 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "你一定要幸福",
+        artist: '简弘亦',
+        url: 'http://m10.music.126.net/20200209220950/0f21edd7132ef2751493fa1492ba0d36/ymusic/5659/0609/510f/a39c417da71c2763b77afe16892fe571.mp3',
+        cover: 'http://img.ytmp3.cn/image/52.jpg',
+      },
+      {
+        name: '一百万个可能(Live)',
+        artist: '摩登兄弟',
+        url: 'http://m10.music.126.net/20200209220950/0f21edd7132ef2751493fa1492ba0d36/ymusic/5659/0609/510f/a39c417da71c2763b77afe16892fe571.mp3',
+        cover: 'http://img.ytmp3.cn/image/53.jpg',
+      },
+      {
+        name: 'The Rose',
+        artist: 'Westlife',
+        url: 'http://m10.music.126.net/20200209220950/0f21edd7132ef2751493fa1492ba0d36/ymusic/5659/0609/510f/a39c417da71c2763b77afe16892fe571.mp3',
+        cover: 'http://img.ytmp3.cn/image/51.jpg',
+      },
+      {
+        name: 'In The Eyes',
+        artist: '江映东',
+        url: 'http://m10.music.126.net/20200209220950/0f21edd7132ef2751493fa1492ba0d36/ymusic/5659/0609/510f/a39c417da71c2763b77afe16892fe571.mp3',
+        cover: 'http://img.ytmp3.cn/image/10.jpg',
+      }
+    ]
+});
